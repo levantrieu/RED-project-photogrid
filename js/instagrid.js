c5235ec44220b621c01818a760f678bce8dc4f11
Levan <user@Macintosh.local>
2016-03-18 16:40:15 -0700

    Add load more button

--- js/instagrid.js
+++ js/instagrid.js
@@ -1,20 +1,24 @@
 $(function () {
   var instaPhotos = '';
+  var nextURL = '';
   var $photos = $('.photos');
+  var $loader = $('.loader');
+  var $loadMore = $('.load-more');
   var $error = $('.error-message');
 
   $('#search-button').on('click', function (event) {
     event.preventDefault();
+    $loader.css('display', 'block');
 
-    $(document).bind('ajaxSend', function () {
-      $('.loader').show();
-    }).bind('ajaxComplete', function () {
-      $('.loader').hide();
-    });
+    // $loadMore.hide();
 
     var hashtag = $('#hashtag').val();
 
+    // animate photo grid scrolling
+
     $('.container').css('height', '8rem').addClass('header-scroll');
+
+    // get photos from API
 
     $.ajax({
       dataType: 'jsonp',
@@ -23,6 +27,45 @@ $(function () {
     })
 
     .done(function (instaData) {
+      nextURL = instaData.pagination.next_url;
+      $.each(instaData.data, function (index, value) {
+        instaPhotos += '<li><div class="instaItem"><a href="' + value.link + '" target="_blank"><img src=' + value.images.standard_resolution.url + '></a>' +
+                       '<div class="user-info"><div class="profile-photo"><img src=' + value.user.profile_picture + '></div>' +
+                       '<div class="user-social"><div class="social-inner"><p>' + value.user.username + '</p>' +
+                       '<i class="fa comments-icon"></i>' + value.comments.count +
+                       '<i class="fa heart-icon"></i>' + value.likes.count + '</div></div></div></div></li>';
+                       console.log('instaData');
+      });
+
+      $photos.empty().append(instaPhotos);
+      instaPhotos = '';
+      $loadMore.show();
+
+
+    })
+
+    .fail(function (instaData) {
+      $error.append('Oops, something went wrong. Please try again.');
+    }).always(function () {
+      $loader.hide();
+    });
+  });
+
+  // Load more photos
+
+  $('.load-more').on('click', function () {
+
+
+    // get photos from API
+
+    $.ajax({
+      dataType: 'jsonp',
+      method: 'GET',
+      url: nextURL,
+    })
+
+    .done(function (instaData) {
+      nextURL = instaData.pagination.next_url;
       $.each(instaData.data, function (index, value) {
         instaPhotos += '<li><div class="instaItem"><a href="' + value.link + '" target="_blank"><img src=' + value.images.standard_resolution.url + '></a>' +
                        '<div class="user-info"><div class="profile-photo"><img src=' + value.user.profile_picture + '></div>' +
@@ -31,12 +74,16 @@ $(function () {
                        '<i class="fa heart-icon"></i>' + value.likes.count + '</div></div></div></div></li>';
       });
 
-      $photos.empty().append(instaPhotos);
+      $photos.append(instaPhotos);
       instaPhotos = '';
+
+
     })
 
     .fail(function (instaData) {
       $error.append('Oops, something went wrong. Please try again.');
+    }).always(function () {
+      $loader.hide();
     });
   });
 });
